bfd30f652ace041b933b7ed9fb60ffb096cc8492
pursuitcomp <pursuitcomp@live.com>
2018-05-23 19:04:19 -0500

    version 1

---
+++ src/com/servlet/LoginServlet.java
@@ -0,0 +1,72 @@
+package com.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.user.Customer;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session=request.getSession(true);
+		Customer cust=(Customer) session.getAttribute("cust");
+		
+		String userName=request.getParameter("user");
+		String password	=request.getParameter("pass");	
+		String error;
+		//String error="You dont have an account";
+		 try {
+	       
+	      
+		if(userName.equals(cust.getUsername())&&password.equals(cust.getPassword())) {
+			 error=" ";
+			 String paid="";
+			 session.setAttribute("payment", paid);
+			response.sendRedirect("myAccount.jsp");
+			
+		}else {
+			
+			response.sendRedirect("Loginpage.jsp");
+			
+		}} catch (NullPointerException e){
+			error="You dont have an account";
+	         response.sendRedirect("Loginpage.jsp");
+	         session.setAttribute("error", error);
+	      }
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
